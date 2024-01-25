38137a2328562bf3bd429b1ec8864c4e844def5a
emrekizilelma <emrekizilelma00@gmail.com>
2024-01-25 14:46:56 +0300

    update

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include <apple.h>
 #include <win32.h>
 
-const void test_linux() {
+/* const void test_linux() {
     printf("You are using linux!\n\n");
 }
 
@@ -20,14 +20,14 @@ const void test_apple() {
 const void test_win32() {
     printf("You are using Windows! \n");
 }
-
+*/
 int main() {
 
     //test_(); // Calling significant function from apple.h which is created on apple.c
 
      #ifdef __linux__
 
-        test_linux();
+        //test_linux();
         get_linux_sys_info();
     
     #elif BSD
